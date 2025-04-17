1e1860e5d060452214e228ba9c9242af3541bf45
emad1711 <mohamedemad17112@gmail.com>
2025-04-17 08:19:47 +0200

    realstateapp

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i$bswj-8nwl8edh4lf8fu9tn+##m%@_y^0^r55nif5ha8a8e-j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['realestate.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['realestate-production-0c92.up.railway.app', 'localhost', '127.0.0.1']
 
 
 
